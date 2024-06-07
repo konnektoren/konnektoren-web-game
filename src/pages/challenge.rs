7a302db625d48868357d8f5f8c6b45158747428c
Christian M <chriamue@gmail.com>
2024-06-07 11:04:27 +0200

    :sparkles: adds challenge result summary

--- src/pages/challenge.rs
+++ src/pages/challenge.rs
@@ -1,5 +1,5 @@
-use konnektoren_core::game::Game;
-use konnektoren_yew::components::challenge::ChallengeComponent;
+use konnektoren_core::{challenges::ChallengeResult, game::Game};
+use konnektoren_yew::components::challenge::{ChallengeComponent, ResultSummaryComponent};
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
@@ -11,13 +11,32 @@ pub struct ChallengePageProps {
 pub fn challenge_page(props: &ChallengePageProps) -> Html {
     let game = Game::default();
 
+    let challenge_result = use_state(|| Option::<ChallengeResult>::None);
+
+    let handle_finish = {
+        let challenge_result = challenge_result.clone();
+        Callback::from(move |result: ChallengeResult| {
+            log::info!("Challenge Result: {:?}", result);
+            challenge_result.set(Some(result.clone()));
+        })
+    };
+
     let challenge = game.create_challenge(&props.id);
 
     match challenge {
         Ok(challenge) => {
+            let result_summary = match &*challenge_result {
+                Some(result) => {
+                    html! {
+                        <ResultSummaryComponent challenge={challenge.clone()} challenge_result={result.clone()} />
+                    }
+                }
+                None => html! {},
+            };
             html! {
                 <div class="challenge-page">
-                    <ChallengeComponent challenge={challenge} />
+                    {result_summary}
+                    <ChallengeComponent challenge={challenge.clone()} on_finish={handle_finish} />
                 </div>
             }
         }
