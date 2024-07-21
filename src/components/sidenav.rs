15d3d4d7e340fcac8474b389b4ee06cff3da6e6d
Christian M <chriamue@gmail.com>
2024-07-21 13:53:08 +0200

    :lipstick: include new icon

--- src/components/sidenav.rs
+++ src/components/sidenav.rs
@@ -22,7 +22,7 @@ pub fn sidenav() -> Html {
         <>
             <div class={sidenav_class}>
                 <button class={ if *is_open {"closebtn"} else  {"openbtn"}} onclick={toggle_sidenav}>{ if *is_open {"×"} else {"☰"} }</button>
-                <Link<Route> to={Route::Home}><Logo img_src={"/favicon.png".to_string()} /></Link<Route>>
+                <Link<Route> to={Route::Home}><Logo img_src={"/assets/images/Logo_Orange_Transparent.svg".to_string()} /></Link<Route>>
                 <div>
                     <Link<Route> to={Route::Profile}>
                         <i class="fa-solid fa-user"></i><span class="link-text">{ i18n!("Profile") }</span>
