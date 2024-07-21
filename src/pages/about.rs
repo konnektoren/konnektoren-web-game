00730d2876a6d82c2843ea957f9d614875f1f387
Christian M <chriamue@gmail.com>
2024-07-21 17:55:40 +0200

    :lipstick: adds konnektoren version1 link to about page

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -1,4 +1,4 @@
-use crate::components::{Footer, SelectLanguage};
+use crate::components::{Footer, Logo, SelectLanguage};
 use crate::i18n;
 use yew::prelude::*;
 
@@ -13,6 +13,18 @@ pub fn about_page() -> Html {
             <p>
                 { i18n!("Through interactive tests and comprehensive examples, this platform aims to enhance your German grammar skills, making you more confident in your language abilities.") }
             </p>
+
+            <h2>{ "Visit Version 1 of Konnektoren" }</h2>
+
+            <p>
+                { "If you would like to visit the first version of Konnektoren, you can do so by clicking the button below." }
+            </p>
+
+            <Logo img_src={"https://version1.konnektoren.help/favicon.png"} />
+
+            <a href="https://version1.konnektoren.help/" target="_blank" rel="noopener noreferrer">
+                <button>{ "Visit Konnektoren v1" }</button>
+            </a>
 
             <SelectLanguage />
 
