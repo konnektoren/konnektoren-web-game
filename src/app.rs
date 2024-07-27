31f6215de1b8d7d6734ddd0cd520dc22ec3def4f
Christian M <chriamue@gmail.com>
2024-07-27 10:15:35 +0200

    :globe_with_meridians: load lang by query

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,13 @@
 use crate::components::Sidenav;
 use crate::pages::{LeaderboardPage, NotFoundPage, ResultsPage};
+use crate::utils::translation::LANGUAGE_KEY;
 use crate::{
     components::Navigation,
     pages::{AboutPage, ChallengePage, HomePage, MapPage, ProfilePage},
     route::Route,
     utils::translation::{translations, LANGUAGES},
 };
+use gloo::storage::{LocalStorage, Storage};
 use yew::prelude::*;
 use yew_i18n::I18nProvider;
 use yew_router::prelude::{BrowserRouter, Switch};
@@ -25,8 +27,31 @@ fn switch_main(route: Route) -> Html {
     }
 }
 
+fn update_language(query: &String) {
+    let query = query.trim_start_matches('?');
+
+    let lang = if !query.is_empty() {
+        query
+            .split('&')
+            .find(|part| part.starts_with("lang="))
+            .and_then(|lang_part| lang_part.split('=').nth(1))
+    } else {
+        None
+    };
+
+    if let Some(lang) = lang {
+        LocalStorage::set(LANGUAGE_KEY, lang).unwrap_or_else(|err| {
+            log::error!("Error setting language in local storage: {:?}", err);
+        });
+    }
+}
+
 #[function_component(App)]
 pub fn app() -> Html {
+    use_effect_with((), |_| {
+        let query = web_sys::window().unwrap().location().search().unwrap();
+        update_language(&query);
+    });
     let supported_languages = LANGUAGES.to_vec();
     let translations = translations();
 
