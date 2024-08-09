3154ba7037db1013b892b61b147163bccafbc258
Christian M <chriamue@gmail.com>
2024-08-09 15:58:20 +0200

    :lipstick: more boarder for speech bubble

--- src/components/speech_bubble.rs
+++ src/components/speech_bubble.rs
@@ -4,9 +4,9 @@ use crate::components::Roulette;
 #[function_component(SpeechBubble)]
 pub fn speech_bubble() -> Html {
     html! {
-        <div class="speech-bubble">
+        <p class="speech-bubble">
             <p>{ "Learn German" }</p>
             <Roulette></Roulette>
-        </div>
+        </p>
     }
 }
