2ded519758b4f0487870f4786b8f690bae9b1c23
Christian M <chriamue@gmail.com>
2024-08-12 10:59:10 +0200

    :sparkles: adds more text to bubble

--- src/components/speech_bubble.rs
+++ src/components/speech_bubble.rs
@@ -1,11 +1,13 @@
+use konnektoren_yew::i18n::use_i18n;
 use crate::components::Roulette;
 use yew::prelude::*;
 
 #[function_component(SpeechBubble)]
 pub fn speech_bubble() -> Html {
+    let i18n = use_i18n();
     html! {
         <p class="speech-bubble">
-            <p>{ "Learn German" }</p>
+            <p>{ i18n.t("Select a german topic to learn with me.") }</p>
             <Roulette></Roulette>
         </p>
     }
