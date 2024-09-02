9469335f02acd845c852dd33dc0ddfe81832cd9f
Christian M <chriamue@gmail.com>
2024-09-02 19:11:56 +0200

    :sparkles: adds language and theme to settings

--- src/pages/settings.rs
+++ src/pages/settings.rs
@@ -1,3 +1,5 @@
+use crate::components::ThemeToggle;
+use konnektoren_yew::components::SelectLanguage;
 use konnektoren_yew::prelude::SettingsComponent;
 use yew::prelude::*;
 
@@ -6,6 +8,8 @@ pub fn settings_page() -> Html {
     html! {
         <div class="settings-page">
             <SettingsComponent />
+            <ThemeToggle />
+            <SelectLanguage />
         </div>
     }
 }
