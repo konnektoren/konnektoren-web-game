4ee945bfe9698a314c4471016af234269387be3b
Christian M <chriamue@gmail.com>
2024-09-24 21:19:36 +0200

    :fire: do not vibrate on other than android chrome

--- src/components/vibrate_effect.rs
+++ src/components/vibrate_effect.rs
@@ -10,12 +10,19 @@ pub fn vibration_effect(props: &VibrateEffectProps) -> Html {
     let duration = props.duration;
 
     use_effect(move || {
-        let duration = duration;
         if let Some(window) = web_sys::window() {
-            if window.navigator().vibrate_with_duration(duration) {
-                log::info!("Vibration effect started");
-            } else {
-                log::error!("Vibration effect failed");
+            let navigator = window.navigator();
+
+            // Detect if the user agent is Chrome on Android
+            if let Some(user_agent) = navigator.user_agent().ok() {
+                if user_agent.contains("Android") && user_agent.contains("Chrome") {
+                    if navigator.vibrate_with_duration(duration) {
+                    } else {
+                        log::error!("Vibration effect failed");
+                    }
+                } else {
+                    log::debug!("Not Chrome on Android, skipping vibration");
+                }
             }
         }
     });
