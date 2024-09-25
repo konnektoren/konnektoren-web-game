c3194d8009eb145eb51e6bd39d7505d183984718
Christian M <chriamue@gmail.com>
2024-09-25 14:12:10 +0200

    :sparkles: adds product to catalog

--- src/pages/marketplace.rs
+++ src/pages/marketplace.rs
@@ -1,11 +1,16 @@
+use konnektoren_core::marketplace::ProductCatalog;
+use konnektoren_yew::components::ProductCatalogComponent;
 use yew::prelude::*;
 
 #[function_component(MarketplacePage)]
 pub fn marketplace_page() -> Html {
+    let product_catalog =
+        ProductCatalog::from_yaml(include_str!("../assets/product_catalog.yml")).unwrap();
+
     html! {
         <div class="marketplace-page">
             <h1>{"Marketplace"}</h1>
-            <p>{"Welcome to the marketplace!"}</p>
+            <ProductCatalogComponent product_catalog={product_catalog} />
         </div>
     }
 }
