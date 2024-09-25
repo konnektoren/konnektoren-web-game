3234f013fb8314e3c1b256db9bddb1fa2d068f24
Christian M <chriamue@gmail.com>
2024-09-25 14:41:50 +0200

    :loud_sound: log selected product

--- src/pages/marketplace.rs
+++ src/pages/marketplace.rs
@@ -7,10 +7,13 @@ pub fn marketplace_page() -> Html {
     let product_catalog =
         ProductCatalog::from_yaml(include_str!("../assets/product_catalog.yml")).unwrap();
 
+    let on_select = Callback::from(|product| {
+        log::info!("Selected product: {:?}", product);
+    });
     html! {
         <div class="marketplace-page">
             <h1>{"Marketplace"}</h1>
-            <ProductCatalogComponent product_catalog={product_catalog} />
+            <ProductCatalogComponent {product_catalog} {on_select} />
         </div>
     }
 }
