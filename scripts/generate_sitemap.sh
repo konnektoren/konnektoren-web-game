065d9e266c09a27b123c34c50c83b3c2a62ed38a
Christian M <chriamue@gmail.com>
2024-10-01 18:19:38 +0200

    :mag: update sitemap generation to generate urls with request parameters

--- scripts/generate_sitemap.sh
+++ scripts/generate_sitemap.sh
@@ -3,8 +3,26 @@
 DOMAINS=("https://konnektoren.help" "https://konnektoren.app")
 CURRENT_DATE=$(date +%Y-%m-%d)
 CURRENT_DATE_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-PAGES=("/" "/map" "/about" "/challenges" "/leaderboard" "/profile" "/marketplace" "/challenge/articles-1"
- "/challenge/reflexivpronoun-1" "/challenge/personal_pronouns-1" "/challenge/konnektoren-1")
+PAGES=("/" "map" "about" "challenges" "leaderboard" "profile" "marketplace")
+CHALLENGES=(
+    "articles-1" "articles-2" "articles-3" "articles-4" "articles-5" "articles-6"
+    "reflexivpronoun-1" "reflexivpronoun-2"
+    "personal_pronouns-1" "personal_pronouns-2"
+    "konnektoren-1" "konnektoren-2" "konnektoren-3" "konnektoren-4" "konnektoren-5"
+    "custom-verbs-1"
+    "custom-perfect_tense-1"
+    "custom-group-of-nouns"
+    "custom-negation"
+    "custom_haupt-neben-satze_exercise"
+    "verbs_mit_dativ_akkusativ"
+    "custom-konjunktiv2"
+    "custom-zeitangaben"
+    "custom-casus"
+    "custom_past_tenses"
+    "custom_verbs_prepositions"
+    "custom_interrogative_particles"
+    "custom-articles-1"
+)
 LANGS=("ar" "cn" "de" "en" "es" "pl" "tr" "ua")
 
 # Function to generate title from URL
@@ -13,7 +31,7 @@ get_title() {
     if [ "$page" = "/" ]; then
         echo "Home"
     else
-        echo "$page" | sed 's/^\///;s/-/ /g;s/\// - /g' | awk '{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1'
+        echo "$page" | sed 's/-/ /g;s/\// - /g' | awk '{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1'
     fi
 }
 
@@ -44,24 +62,26 @@ EOF
 
     # Loop through each page and generate the corresponding sitemap and atom entries
     for PAGE in "${PAGES[@]}"; do
+      URL_SUFFIX=$([ "$PAGE" = "/" ] && echo "" || echo "?page=${PAGE}")
       TITLE=$(get_title "$PAGE")
 
       # Sitemap entry
       cat <<EOF >> sitemap_${DOMAIN_NAME}.xml
   <url>
-    <loc>${DOMAIN}${PAGE}</loc>
+    <loc>${DOMAIN}${URL_SUFFIX}</loc>
     <lastmod>${CURRENT_DATE}</lastmod>
-    <xhtml:link rel="alternate" hreflang="en" href="${DOMAIN}${PAGE}"/>
+    <xhtml:link rel="alternate" hreflang="en" href="${DOMAIN}${URL_SUFFIX}"/>
 EOF
 
-      echo "${DOMAIN}${PAGE}" >> sitemap_${DOMAIN_NAME}.txt
+      echo "${DOMAIN}${URL_SUFFIX}" >> sitemap_${DOMAIN_NAME}.txt
 
       for LANG in "${LANGS[@]}"; do
+        LANG_SUFFIX=$([ "$URL_SUFFIX" = "" ] && echo "?lang=${LANG}" || echo "${URL_SUFFIX}&lang=${LANG}")
         cat <<EOF >> sitemap_${DOMAIN_NAME}.xml
-    <xhtml:link rel="alternate" hreflang="${LANG}" href="${DOMAIN}${PAGE}?lang=${LANG}"/>
+    <xhtml:link rel="alternate" hreflang="${LANG}" href="${DOMAIN}${LANG_SUFFIX}"/>
 EOF
 
-        echo "${DOMAIN}${PAGE}?lang=${LANG}" >> sitemap_${DOMAIN_NAME}.txt
+        echo "${DOMAIN}${LANG_SUFFIX}" >> sitemap_${DOMAIN_NAME}.txt
       done
 
       echo "  </url>" >> sitemap_${DOMAIN_NAME}.xml
@@ -70,8 +90,46 @@ EOF
       cat <<EOF >> atom_${DOMAIN_NAME}.xml
   <entry>
     <title>${TITLE}</title>
-    <link href="${DOMAIN}${PAGE}"/>
-    <id>${DOMAIN}${PAGE}</id>
+    <link href="${DOMAIN}${URL_SUFFIX}"/>
+    <id>${DOMAIN}${URL_SUFFIX}</id>
+    <updated>${CURRENT_DATE_TIME}</updated>
+    <summary>Learn about ${TITLE} on ${DOMAIN_NAME}</summary>
+  </entry>
+EOF
+    done
+
+    # Add challenge pages
+    for CHALLENGE in "${CHALLENGES[@]}"; do
+      URL_SUFFIX="?page=challenge&id=${CHALLENGE}"
+      TITLE=$(get_title "Challenge ${CHALLENGE}")
+
+      # Sitemap entry
+      cat <<EOF >> sitemap_${DOMAIN_NAME}.xml
+  <url>
+    <loc>${DOMAIN}${URL_SUFFIX}</loc>
+    <lastmod>${CURRENT_DATE}</lastmod>
+    <xhtml:link rel="alternate" hreflang="en" href="${DOMAIN}${URL_SUFFIX}"/>
+EOF
+
+      echo "${DOMAIN}${URL_SUFFIX}" >> sitemap_${DOMAIN_NAME}.txt
+
+      for LANG in "${LANGS[@]}"; do
+        LANG_SUFFIX="${URL_SUFFIX}&lang=${LANG}"
+        cat <<EOF >> sitemap_${DOMAIN_NAME}.xml
+    <xhtml:link rel="alternate" hreflang="${LANG}" href="${DOMAIN}${LANG_SUFFIX}"/>
+EOF
+
+        echo "${DOMAIN}${LANG_SUFFIX}" >> sitemap_${DOMAIN_NAME}.txt
+      done
+
+      echo "  </url>" >> sitemap_${DOMAIN_NAME}.xml
+
+      # Atom entry
+      cat <<EOF >> atom_${DOMAIN_NAME}.xml
+  <entry>
+    <title>${TITLE}</title>
+    <link href="${DOMAIN}${URL_SUFFIX}"/>
+    <id>${DOMAIN}${URL_SUFFIX}</id>
     <updated>${CURRENT_DATE_TIME}</updated>
     <summary>Learn about ${TITLE} on ${DOMAIN_NAME}</summary>
   </entry>
