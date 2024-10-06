ef10cf44b36b233aeefb1ea06b9477142ec03a4b
Christian M <chriamue@gmail.com>
2024-10-06 14:00:31 +0200

    :bug::lipstick: fix challenge effect animation

--- src/components/challenge/challenge_effect.rs
+++ src/components/challenge/challenge_effect.rs
@@ -1,11 +1,10 @@
 use crate::components::{Chat, VibrateEffectComponent};
 use crate::utils::translation::i18n_macro::selected_language;
+use gloo::timers::callback::Timeout;
 use konnektoren_core::challenges::ChallengeVariant;
 use konnektoren_core::prelude::{Challenge, ChallengeResult};
 use konnektoren_yew::components::challenge::ChallengeEvent;
 use konnektoren_yew::components::{ChallengeComponent, MusicComponent};
-use konnektoren_yew::effects::BlinkAnimation;
-use std::time::Duration;
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
@@ -48,31 +47,43 @@ pub fn challenge_effect_component(props: &Props) -> Html {
             ChallengeEvent::SolvedCorrect(_index) => {
                 let c = *counter + 1;
                 counter.set(c);
-                effect_ref.set(html! {
+                let effect = html! {
                     <>
-                    <MusicComponent id={format!("challenge-effect__music--correct-{}", c)} url="/music/UI Positive Signal 002.wav" repeat={false} />
-                    <BlinkAnimation target_id={format!("challenge-effect__blink--correct-{}", c)} duration={Duration::from_millis(800)} color={"green"} />
+                        <div class="challenge-effect__blink challenge-effect__blink--correct"></div>
+                        <MusicComponent id={format!("challenge-effect__music--correct-{}", c)} url="/music/UI Positive Signal 002.wav" repeat={false} />
                     </>
-                });
+                };
+                effect_ref.set(effect);
+                let effect_ref_clone = effect_ref.clone();
+                Timeout::new(2000, move || {
+                    effect_ref_clone.set(html! { <div></div> });
+                })
+                .forget();
             }
             ChallengeEvent::SolvedIncorrect(_index) => {
                 let c = *counter + 1;
                 counter.set(c);
-                effect_ref.set(html! {
+                let effect = html! {
                     <>
-                    <MusicComponent id={format!("challenge-effect__music--incorrect-{}", c)} url="/music/UI Negative Signal 003.wav" repeat={false} />
-                    <BlinkAnimation target_id={format!("challenge-effect__blink--incorrect-{}", c)} duration={Duration::from_millis(800)} color={"red"} />
-                    <VibrateEffectComponent duration={100} key={c} />
+                        <div class="challenge-effect__blink challenge-effect__blink--incorrect"></div>
+                        <MusicComponent id={format!("challenge-effect__music--incorrect-{}", c)} url="/music/UI Negative Signal 003.wav" repeat={false} />
+                        <VibrateEffectComponent duration={100} key={c} />
                     </>
-                });
+                };
+                effect_ref.set(effect);
+                let effect_ref_clone = effect_ref.clone();
+                Timeout::new(2000, move || {
+                    effect_ref_clone.set(html! { <div></div> });
+                })
+                .forget();
             }
         })
     };
 
     html! {
         <div class="challenge-effect" id={format!("challenge-effect-{}", *counter)}>
+            {(*effect_ref).clone()}
             <div class="challenge-effect__content">
-                {(*effect_ref).clone()}
                 <ChallengeComponent
                     challenge={challenge.clone()}
                     variant={props.variant.clone()}
