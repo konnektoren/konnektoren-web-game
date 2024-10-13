b302cb7e0111a81bdb4a27bc34d1f3d80a013a7e
Christian M <chriamue@gmail.com>
2024-10-13 11:54:19 +0200

    :sparkles: adds routing main

--- routing/src/pages/welcome.rs
+++ routing/src/pages/welcome.rs
@@ -1,12 +1,18 @@
+use crate::route::Route;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component(WelcomePage)]
 pub fn welcome_page() -> Html {
     html! {
         <div class="welcome-page">
+            <Link<Route> to={Route::Home}>
+            <>
             <h1>{ "Welcome to Konnektoren" }</h1>
             <p>{ "Your path to german grammar." }</p>
-            <a href="/">{ "Start" }</a>
+            {"Let's start"}
+            </>
+            </Link<Route>>
         </div>
     }
 }
