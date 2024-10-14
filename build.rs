b64370573e916a6deb1daae114a5721ca5e5cb16
Christian M <chriamue@gmail.com>
2024-10-14 18:04:56 +0200

    :recycle::wrench: add chat api url to build env

--- build.rs
+++ build.rs
@@ -2,5 +2,8 @@ use std::env;
 
 fn main() {
     let base_path = env::var("BASE_PATH").unwrap_or_else(|_| String::from(""));
+    let chat_api_url = env::var("KONNEKTOREN_CHAT_API_URL")
+        .unwrap_or_else(|_| String::from("https://api.konnektoren.help/api/v1/chat"));
     println!("cargo:rustc-env=BASE_PATH={}", base_path);
+    println!("cargo:rustc-env=KONNEKTOREN_CHAT_API_URL={}", chat_api_url);
 }
--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 pub const BASE_PATH: &str = env!("BASE_PATH");
+pub const CHAT_API_URL: &str = env!("KONNEKTOREN_CHAT_API_URL");
 
 pub fn full_path(path: &str) -> String {
     format!("{}{}", BASE_PATH, path)
