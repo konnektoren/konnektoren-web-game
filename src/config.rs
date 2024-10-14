6f530b63f54cb5842da33886a80a640f754b6d6f
Christian M <chriamue@gmail.com>
2024-10-14 18:56:43 +0200

    :wrench: move payment consts to build env

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,8 @@
 pub const BASE_PATH: &str = env!("BASE_PATH");
 pub const CHAT_API_URL: &str = env!("KONNEKTOREN_CHAT_API_URL");
+pub const TON_API_URL: &str = env!("KONNEKTOREN_TON_API_URL");
+pub const TONCONNECT_MANIFEST_URL: &str = env!("KONNEKTOREN_TONCONNECT_MANIFEST_URL");
+pub const PAYMENT_ADDRESS: &str = env!("KONNEKTOREN_PAYMENT_ADDRESS");
 
 pub fn full_path(path: &str) -> String {
     format!("{}{}", BASE_PATH, path)
