bb4a98c74271bcc03143a9bfc6ffa37837410111
Christian M <chriamue@gmail.com>
2024-10-14 19:39:24 +0200

    :wrench: use settings repository for tour button

--- src/components/tour_button.rs
+++ src/components/tour_button.rs
@@ -1,5 +1,5 @@
 use crate::components::Tour;
-use gloo::storage::{LocalStorage, Storage};
+use crate::repository::{LocalStorage, Repository, SettingsRepository, SETTINGS_STORAGE_KEY};
 use konnektoren_yew::i18n::use_i18n;
 use yew::prelude::*;
 
@@ -13,17 +13,48 @@ pub fn tour_button(props: &Props) -> Html {
     let i18n = use_i18n();
     let show_tour = use_state(|| false);
 
-    let show_tour_button =
-        use_state(|| LocalStorage::get::<bool>(format!("{}-show", props.id)).unwrap_or(true));
+    let show_helpers = use_state(|| true);
+
+    {
+        let show_helpers = show_helpers.clone();
+        use_effect_with((), move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let settings_repository =
+                    SettingsRepository::new(LocalStorage::new(Some(SETTINGS_STORAGE_KEY)));
+
+                let stored_settings = settings_repository
+                    .get(SETTINGS_STORAGE_KEY)
+                    .await
+                    .unwrap_or_default()
+                    .unwrap_or_default();
+                show_helpers.set(stored_settings.show_helpers);
+            });
+        });
+    }
 
     let on_click = {
         let show_tour = show_tour.clone();
         Callback::from(move |_| {
             show_tour.set(true);
+            wasm_bindgen_futures::spawn_local(async move {
+                let settings_repository =
+                    SettingsRepository::new(LocalStorage::new(Some(SETTINGS_STORAGE_KEY)));
+
+                let mut stored_settings = settings_repository
+                    .get(SETTINGS_STORAGE_KEY)
+                    .await
+                    .unwrap_or_default()
+                    .unwrap_or_default();
+                stored_settings.show_helpers = false;
+                settings_repository
+                    .save(SETTINGS_STORAGE_KEY, &stored_settings)
+                    .await
+                    .unwrap();
+            });
         })
     };
 
-    match (*show_tour_button, *show_tour) {
+    match (*show_helpers, *show_tour) {
         (true, false) => {
             html! {
                 <div class="tour-button">
--- src/components/tour_toggle.rs
+++ src/components/tour_toggle.rs
@@ -1,27 +1,49 @@
-use gloo::storage::{LocalStorage, Storage};
+use crate::repository::{LocalStorage, Repository, SettingsRepository, SETTINGS_STORAGE_KEY};
 use konnektoren_yew::i18n::use_i18n;
 use yew::prelude::*;
-
-const TOUR_BUTTON_KEY: &str = "main-show";
 
 #[function_component(TourToggle)]
 pub fn tour_toggle() -> Html {
     let i18n = use_i18n();
-    let tour_button_visible =
-        use_state(|| LocalStorage::get::<bool>(TOUR_BUTTON_KEY).unwrap_or(true));
+    let show_helpers = use_state(|| true);
 
     {
-        let tour_button_visible = tour_button_visible.clone();
-        use_effect_with((*tour_button_visible).clone(), move |_| {
-            LocalStorage::set(TOUR_BUTTON_KEY, *tour_button_visible).unwrap();
-            || ()
+        let show_helpers = show_helpers.clone();
+        use_effect_with((), move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let settings_repository =
+                    SettingsRepository::new(LocalStorage::new(Some(SETTINGS_STORAGE_KEY)));
+
+                let stored_settings = settings_repository
+                    .get(SETTINGS_STORAGE_KEY)
+                    .await
+                    .unwrap_or_default()
+                    .unwrap_or_default();
+                show_helpers.set(stored_settings.show_helpers);
+            });
         });
     }
 
-    let toggle_tour_button = {
-        let tour_button_visible = tour_button_visible.clone();
+    let toggle_show_helpers = {
+        let show_helpers = show_helpers.clone();
         Callback::from(move |_| {
-            tour_button_visible.set(!*tour_button_visible);
+            let new_show_helpers = !*show_helpers;
+            show_helpers.set(new_show_helpers);
+            wasm_bindgen_futures::spawn_local(async move {
+                let settings_repository =
+                    SettingsRepository::new(LocalStorage::new(Some(SETTINGS_STORAGE_KEY)));
+
+                let mut stored_settings = settings_repository
+                    .get(SETTINGS_STORAGE_KEY)
+                    .await
+                    .unwrap_or_default()
+                    .unwrap_or_default();
+                stored_settings.show_helpers = new_show_helpers;
+                settings_repository
+                    .save(SETTINGS_STORAGE_KEY, &stored_settings)
+                    .await
+                    .unwrap();
+            });
         })
     };
 
@@ -29,8 +51,8 @@ pub fn tour_toggle() -> Html {
         <div class="tour-toggle">
             <label>
                 <input type="checkbox"
-                    checked={*tour_button_visible}
-                    onclick={toggle_tour_button.clone()} />
+                    checked={*show_helpers}
+                    onclick={toggle_show_helpers.clone()} />
                 { i18n.t("Show Tour Button")}
             </label>
         </div>
