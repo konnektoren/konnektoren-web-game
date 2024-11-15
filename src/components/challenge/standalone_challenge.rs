ca6a250e44f156495f0460ddb7426a714cd1c667
Christian M <chriamue@gmail.com>
2024-11-15 15:57:04 +0100

    :sparkles: adds standalone challenge

---
+++ src/components/challenge/standalone_challenge.rs
@@ -0,0 +1,81 @@
+use konnektoren_core::challenges::{ChallengeResult, Timed};
+use konnektoren_core::commands::{ChallengeCommand, Command};
+use konnektoren_core::prelude::Game;
+use konnektoren_yew::components::ChallengeComponent;
+use konnektoren_yew::i18n::use_selected_language;
+use yew::prelude::*;
+
+use crate::model::LevelLoader;
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub challenge_id: String,
+    #[prop_or_default]
+    pub on_finish: Option<Callback<ChallengeResult>>,
+}
+
+#[function_component(StandaloneChallengeComponent)]
+pub fn standalone_challenge(props: &Props) -> Html {
+    let game = Game::level_a1().unwrap();
+    let language = use_selected_language();
+
+    let challenge_state = use_state(|| game.create_challenge(&props.challenge_id).ok());
+
+    {
+        let challenge_id = props.challenge_id.clone();
+        let game = game.clone();
+        let challenge_state = challenge_state.clone();
+
+        use_effect_with(challenge_id, move |id| {
+            if let Ok(mut challenge) = game.create_challenge(id) {
+                challenge.start();
+                challenge_state.set(Some(challenge));
+            }
+            || ()
+        });
+    }
+
+    let handle_command = {
+        let on_finish = props.on_finish.clone();
+
+        Callback::from(move |command: Command| {
+            if let Command::Challenge(ChallengeCommand::Finish(Some(result))) = command {
+                if let Some(on_finish) = on_finish.as_ref() {
+                    on_finish.emit(result);
+                }
+            }
+        })
+    };
+
+    match (*challenge_state).clone() {
+        Some(challenge) => {
+            if let Some(config) = game.game_paths[0].get_challenge_config(&props.challenge_id) {
+                html! {
+                    <div class="standalone-challenge">
+                        <ChallengeComponent
+                            challenge={challenge}
+                            variant={config.variant.clone()}
+                            on_command={handle_command}
+                            language={language.get()}
+                        />
+                    </div>
+                }
+            } else {
+                html! {
+                    <div class="standalone-challenge">
+                        <div class="error">
+                            {"Failed to load challenge configuration"}
+                        </div>
+                    </div>
+                }
+            }
+        }
+        None => html! {
+            <div class="standalone-challenge">
+                <div class="error">
+                    {"Failed to load challenge"}
+                </div>
+            </div>
+        },
+    }
+}
