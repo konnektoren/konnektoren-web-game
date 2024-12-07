a02b77fc50a8558cd07a10bfa350354fe69fcc17
Christian M <chriamue@gmail.com>
2024-12-07 10:59:06 +0100

    :globe_with_meridians: remove forward on lang

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,7 +1,5 @@
 use crate::components::{Chat, FeedbackPopup, Logo, SpeechBubble, TourButton};
-use crate::utils::translation::LANGUAGE_KEY;
 use crate::Route;
-use gloo::storage::{LocalStorage, Storage};
 use gloo::utils::document;
 use konnektoren_yew::i18n::use_i18n;
 use konnektoren_yew::managers::ProfilePointsManager;
@@ -12,41 +10,25 @@ use yew_router::prelude::Link;
 #[function_component]
 pub fn HomePage() -> Html {
     let i18n = use_i18n();
-    let language =
-        use_state(|| LocalStorage::get(LANGUAGE_KEY).unwrap_or_else(|_| "en".to_string()));
-    {
-        let language = language.clone();
-        let i18n = i18n.clone();
-        use_effect_with(language.clone(), move |_| {
-            document().set_title(&format!(
-                "Konnektoren - {}",
-                i18n.t("Your Adventure in German Grammar")
-            ));
-            || ()
-        });
-    }
+
+    let title = format!(
+        "Konnektoren - {}",
+        i18n.t("Your Adventure in German Grammar")
+    );
+    use_effect(move || {
+        document().set_title(&title);
+        || ()
+    });
 
     let navigator = use_navigator().unwrap();
     {
         let navigator = navigator.clone();
-        let language = language.clone();
         use_effect_with((), move |_| {
             let query = web_sys::window()
                 .unwrap()
                 .location()
                 .search()
                 .unwrap_or_default();
-            let search_params = web_sys::UrlSearchParams::new_with_str(&query).unwrap();
-
-            if let Some(lang) = search_params.get("lang") {
-                if lang != *language {
-                    LocalStorage::set(LANGUAGE_KEY, lang.clone()).unwrap_or_else(|err| {
-                        log::error!("Error setting language in local storage: {:?}", err);
-                    });
-                    language.set(lang);
-                }
-            }
-
             let route = Route::from(query.as_str());
             navigator.replace(&route);
 
