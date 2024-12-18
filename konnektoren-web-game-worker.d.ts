45dd50aff94cbb2f745a2a11783c5e71bf5a8c5e
chriamue <chriamue@users.noreply.github.com>
2024-12-18 20:03:53 +0000

    deploy: 796d4b8630cb2b87e1d1d3672f56c616a844af02

--- konnektoren-web-game-worker.d.ts
+++ konnektoren-web-game-worker.d.ts
@@ -17,7 +17,7 @@ export interface InitOutput {
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_export_7: WebAssembly.Table;
   readonly __externref_table_dealloc: (a: number) => void;
-  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6a948b7ff858c86b: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0c473f02292fbb38: (a: number, b: number) => void;
   readonly closure2938_externref_shim: (a: number, b: number, c: any) => void;
   readonly closure3464_externref_shim: (a: number, b: number, c: any) => void;
   readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he6a26b60c2163235: (a: number, b: number) => void;
